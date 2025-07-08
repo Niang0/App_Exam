755815c896813d26859ec6cf76e1956d9c9aef0d
Niang0 <abdoukillmonger@gmail.com>
2025-07-08 21:45:36 +0000

    misàjour10

--- dashboard/visualisations.py
+++ dashboard/visualisations.py
@@ -1,10 +1,6 @@
+import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
-import pandas as pd
-
-import streamlit as st
-import matplotlib.pyplot as plt
-import pandas as pd
 
 def afficher_dashboard(df, titre):
     st.subheader(f"📊 Dashboard - {titre}")
